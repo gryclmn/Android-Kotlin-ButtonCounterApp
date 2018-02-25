41a6bdfa3a55bcbe4289bf9d8a79896214720112
gryclmn <gryclmn@hotmail.com>
2018-02-25 15:21:28 -0800

    Continuing learning

--- app/src/main/java/com/devbygc/buttonclickapp/MainActivity.kt
+++ app/src/main/java/com/devbygc/buttonclickapp/MainActivity.kt
@@ -2,30 +2,31 @@ package com.devbygc.buttonclickapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.method.ScrollingMovementMethod
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
-    private var userInput: EditText? = null
-    private var button: Button? = null
     private var textView: TextView? = null
-    private var numTimesClicked = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userInput = findViewById<EditText>(R.id.editText)
-        button = findViewById<Button>(R.id.button)
+        val userInput: EditText = findViewById<EditText>(R.id.editText)
+        val button: Button = findViewById<Button>(R.id.button)
         textView = findViewById<TextView>(R.id.textView)
+        textView?.text = ""
+        textView?.movementMethod = ScrollingMovementMethod()
+        userInput.setText("")
 
-        button?.setOnClickListener(object : View.OnClickListener {
+        button.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
-                numTimesClicked += 1
-                textView?.append("\nThe button got tapped $numTimesClicked time")
+                textView?.append(userInput.text)
+                textView?.append("\n")
+                userInput.text.clear()
             }
         })
     }
